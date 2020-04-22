6c251de972edf1651230506fb4ffa24b6728a886
Krstan Vjestica <krle@tilda.center>
2020-04-22 12:51:38 +0200

    its assemble scripts to one

--- bsd.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-NAME=$USER
-
-sudo pkg install -y git-lite cmake tmux ttyd
-
-tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 bash -x"
-tmux split-window -h -p 50 -t 0 "ssh -R localhost:80:localhost:8080 $NAME@ssh.localhost.run"
-tmux a -t "set ssh in browser"
