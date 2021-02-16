ba54e332fc5a6dc16de22ace1785702774f63bcc
Krstan Vjestica <krle@tilda.center>
2021-02-16 10:57:31 +0100

    optimization for tmux part of work

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh 
 
 check_or_create(){
 FILE=~/.ssh/id_dsa
@@ -23,12 +23,30 @@ echo "Your username $USER and your password only you know"
 
 UNAME=$(uname)
 
+tmux_job(){
+
+    ## If want to enable authentication and readonly just uncomment this:
+    tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -c $USER:$PASSWD -B -R bash -lic 'tmux a'"
+    ## and comment default one
+
+    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 bash x"
+    ## If you want to be 'readonly' just add -R. Example:
+    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -R bash x"
+
+    tmux split-window -h -p 50 -t 0 "ssh -R 80:localhost:8080 ssh.localhost.run"
+    tmux split-window -v
+    tmux a -t "set ssh in browser"
+
+}
+
+
 
 PKG_PACKAGE_NAME="git-lite cmake tmux ttyd"
 DEB_PACKAGE_NAME="git cmake make tmux build-essential libjson-c-dev libwebsockets-dev"
 DNF_PACKAGE_NAME="git cmake.x86_64 make tmux libjson-rpc-cpp-devel.x86_64 libwebsockets-devel.x86_64 json-c-devel.x86_64 openssl-devel.x86_64 zlib-devel.x86_64"
 
  if cat /etc/*release | grep ^NAME | grep Fedora; then
+   check_or_create
     echo "==============================================="
     echo "Installing packages $DNF_PACKAGE_NAME on Fedora"
     echo "==============================================="
@@ -39,24 +57,15 @@ DNF_PACKAGE_NAME="git cmake.x86_64 make tmux libjson-rpc-cpp-devel.x86_64 libweb
     cd ttyd
     mkdir build
     cd build
-    cmake ..
+    sudo cmake ..
     sudo make && sudo make install
     cd ../../
     rm -rf ttyd/
+   tmux_job
 
-    ## If want to enable authentication and readonly just uncomment this:
-    tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -c $USER:$PASSWD -B -R bash -lic 'tmux a'"
-    ## and comment default one
-
-    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 bash x"
-    ## If you want to be 'readonly' just add -R. Example:
-    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -R bash x"
-
-    tmux split-window -v
-    tmux split-window -h -p 50 -t 0 "ssh -R 80:localhost:8080 ssh.localhost.run"
-    tmux a -t "set ssh in browser"
 
  elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
+   check_or_create
     echo "==============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Ubuntu"
     echo "==============================================="
@@ -73,17 +82,7 @@ DNF_PACKAGE_NAME="git cmake.x86_64 make tmux libjson-rpc-cpp-devel.x86_64 libweb
     cd ../../
     rm -rf ttyd/
 
-    ## If want to enable authentication and readonly just uncomment this:
-    tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -c $USER:$PASSWD -B -R bash -lic 'tmux a'"
-    ## and comment default one
-
-    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 bash x"
-    ## If you want to be 'readonly' just add -R. Example:
-    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -R bash x"
-
-    tmux split-window -v
-    tmux split-window -h -p 50 -t 0 "ssh -R 80:localhost:8080 ssh.localhost.run"
-    tmux a -t "set ssh in browser"
+   tmux_job
 
  elif cat /etc/*release | grep ^NAME | grep Debian; then
    check_or_create
@@ -102,37 +101,18 @@ DNF_PACKAGE_NAME="git cmake.x86_64 make tmux libjson-rpc-cpp-devel.x86_64 libweb
     sudo make && sudo make install
     cd ../../
     rm -rf ttyd/
+  tmux_job
 
-    ## If want to enable authentication and readonly just uncomment this:
-    tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -c $USER:$PASSWD -B -R bash -lic 'tmux a'"
-    ## and comment default one
-
-    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 bash x"
-    ## If you want to be 'readonly' just add -R. Example:
-    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -R bash x"
-
-    tmux split-window -h -p 50 -t 0 "ssh -R 80:localhost:8080 ssh.localhost.run"
-    tmux split-window -v
-    tmux a -t "set ssh in browser"
 
  elif uname -a | awk '{ print $1}' | grep FreeBSD; then
+   check_or_create
     echo "================================================="
     echo "Installing packages $PKG_PACKAGE_NAME on FreeBSD"
     echo "================================================="
     sudo pkg update
     sudo pkg install -y $PKG_PACKAGE_NAME
 
-    ## If want to enable authentication and readonly just uncomment this:
-    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -c $USER:$PASSWD -B -R bash -lic 'tmux a'"
-    ## and comment default one
-
-    tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 bash x"
-    ## If you want to be 'readonly' just add -R. Example:
-    #tmux new-session -s "set ssh in browser" -d "ttyd -p 8080 -R bash x"
-
-    tmux split-window -v
-    tmux a -t "set ssh in browser"
-    tmux split-window -h -p 30 -t 0 "ssh -R 80:localhost:8080 ssh.localhost.run"
+  tmux_job
 
  else
     echo "OS NOT DETECTED, couldn't install package $PACKAGE"
